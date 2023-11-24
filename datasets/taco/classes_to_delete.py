99a29ea5048b1b14e408e48b0bfec2483fe4b6a3
ethan <ethan.fifle@gmail.com>
2023-11-24 09:35:28 -0500

    Yolov8m @ 400 epochs Augmented

---
+++ datasets/taco/classes_to_delete.py
@@ -0,0 +1,25 @@
+import os
+
+folder_path = 'datacleanup'
+class_id = 1  # Class id to look for
+
+# Iterate through files in the folder
+for filename in os.listdir(folder_path):
+    file_path = os.path.join(folder_path, filename)
+
+    # Open the file and read all lines
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    # Filter out lines starting with the specified class_id
+    filtered_lines = []
+    for line in lines:
+        # Check if the line starts with the specified class_id
+        if not (line.startswith(str(class_id) + ' ') or line == str(class_id)):
+            filtered_lines.append(line.strip())
+
+    # Write the updated lines back to the file
+    with open(file_path, 'w') as updated_file:
+        updated_file.write('\n'.join(filtered_lines))
+
+print("Finished updating files.")
